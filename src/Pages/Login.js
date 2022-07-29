d1b64a1beddafd85658e2c6e9b0dca9bf61c7b7c
info <58048398+kaushikappani@users.noreply.github.com>
2022-07-30 01:58:54 +0530

    UI

--- src/Pages/Login.js
+++ src/Pages/Login.js
@@ -1,19 +1,41 @@
-import { Grid } from '@mui/material'
-import { Container } from '@mui/system'
-import React from 'react'
-import Navbar from '../Components/Navbar'
+import { Grid, TextField, Typography } from "@mui/material";
+import { Container } from "@mui/system";
+import React from "react";
+import Navbar from "../Components/Navbar";
 
 const Login = () => {
   return (
-      <div>
-          <Navbar auth={false} />
-          <Container>
-              <Grid container>
-                  <Grid item> </Grid>
-              </Grid>
-          </Container>
+    <div className = "loginDiv">
+      <Navbar auth={false} />
+      <Container className="pagecontainer">
+        <Grid container>
+          <Grid item xs={12} sm={12} md={6}>
+            <Typography variant="h3">Shopkart</Typography>
+            <Typography variant="caption">
+              Stay fly in your high heels.
+            </Typography>
+          </Grid>
+          <Grid item xs={12} sm={12} md={6} style={{marginTop:"15px"}}>
+            <TextField
+              style={{ width: "100%" }}
+              helperText="Please enter your email"
+              id="email"
+              label="Name"
+              type="email"
+            />
+            <br />
+            <TextField
+              style={{ width: "100%" }}
+              helperText="Please enter your password"
+              id="password"
+              label="Name"
+              type="password"
+            />
+          </Grid>
+        </Grid>
+      </Container>
     </div>
-  )
-}
+  );
+};
 
-export default Login
+export default Login;
