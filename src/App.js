b69079012c7bf66dbc3fd014e09975f6b04bacf5
Appani Kaushik <58048398+kaushikappani@users.noreply.github.com>
2022-07-30 09:40:55 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,16 +1,18 @@
-import React,{} from "react";
+//imports
+import React from "react";
 import Products from "./Pages/Products";
 import { Routes, Route, BrowserRouter } from "react-router-dom";
 import DetailPage from "./Pages/DetailPage";
 import Cart from "./Pages/Cart";
 import Login from "./Pages/Login";
 
+// App Component Which routes pages
+
 function App() {
 
   return (
     <BrowserRouter>
       <Routes>
-        
         <Route path="/" element={<Products />} />
         <Route path="/login" element={<Login />} />
         <Route path="/product/:id" element={<DetailPage />} />
